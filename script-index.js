5925c7e99e56256b61f7f52b95b7ce131c45d034
Matas <matas.jarilinas@stud.vgtu.lt>
2019-09-17 16:07:06 +0300

    pirmasis puslapis su JSON failu, saugojimu į local storage ir AJAX užklausa

---
+++ script-index.js
@@ -0,0 +1,34 @@
+const ul = document.querySelector("ul");
+const saveList = document.getElementById("userButton");
+const input = document.getElementById("userInput");
+const inputStorage = document.getElementById("saveDataInLocalStorage");
+
+let obj;
+
+const saveToLocalStorage = () =>{
+    window.localStorage.setItem('users', JSON.stringify(obj));
+}
+
+function createListElement() {
+	let li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    obj.push({"name": input.value});
+	input.value = "";
+}
+
+const takeDataFromJson = () =>{
+    fetch("./klientai.json")
+    .then(resp=> resp.json())
+    .then(data => {
+        obj = data
+    });
+}
+
+takeDataFromJson();
+
+console.log(obj)
+
+saveList.addEventListener("click", createListElement);
+inputStorage.addEventListener("click", saveToLocalStorage);
+
