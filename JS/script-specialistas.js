cba78544940f0d77c2b1073d86659e13bd6f0d1c
Matas <matas.jarilinas@stud.vgtu.lt>
2019-09-18 15:16:07 +0300

    pridįti mygtukai specialisto puslapiui

--- JS/script-specialistas.js
+++ JS/script-specialistas.js
@@ -2,8 +2,19 @@ const ul = document.getElementById("list")
 
 const obj = JSON.parse(window.localStorage.getItem('users'));
 
+function removeParent(evt){
+        
+    let span = document.createElement("span")
+    span.appendChild(document.createTextNode("Aptarnautas"))
+    evt.target.parentElement.appendChild(span)
+
+    evt.target.remove()
+    
+}
+
 obj.map(data => {
     let sort = data.klientai
+    sort.sort((a, b) => (a.nr > b.nr) ? 1 : -1)
     let h1 = document.createElement("h1")
     h1.appendChild(document.createTextNode(data.specialistas))
     ul.appendChild(h1)
@@ -11,5 +22,11 @@ obj.map(data => {
         let li = document.createElement("li")
         li.appendChild(document.createTextNode("Numeris eilėje: "+klientas.nr+ ". Vardas: " + klientas.name))
         ul.appendChild(li)
+
+        let button = document.createElement("button");
+        button.appendChild(document.createTextNode("Aptarnauta"));
+        li.appendChild(button);
+        
+        button.onclick = removeParent;
     })
 })
